8c4f02f637592bfb81761128a1fe3f88f21d78f0
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-06-22 17:48:40 +0200

    Add work on blog project : translate comments in english, add an MVC architecture, add a template, add an folder structure, add a addComment feature

--- TD_MVC/2-blog _split/affichageAccueil.php
+++ TD_MVC/2-blog _split/affichageAccueil.php
@@ -10,20 +10,8 @@
         <h1>Mon super blog !</h1>
         <p>Derniers billets du blog :</p>
  
+        
         <?php
-        // Connexion à la base de données
-        try
-        {
-        	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-        }
-        catch(Exception $e)
-        {
-                die('Erreur : '.$e->getMessage());
-        }
-
-        // On récupère les 5 derniers billets
-        $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
         while ($donnees = $req->fetch())
         {
         ?>
@@ -35,7 +23,6 @@
             
             <p>
             <?php
-            // On affiche le contenu du billet
             echo nl2br(htmlspecialchars($donnees['contenu']));
             ?>
             <br />
@@ -43,7 +30,7 @@
             </p>
         </div>
         <?php
-        } // Fin de la boucle des billets
+        }
         $req->closeCursor();
         ?>
     </body>
